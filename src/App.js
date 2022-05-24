3c2073d704a88e3ffe4390914e8bfa6c82a3c091
Vlad <sotinvlad@yandex.ru>
2022-05-24 14:50:58 +0300

    Верстка шапки

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import pizzaLogo from './assets/pizza-logo.png';
+import shopCart from './assets/shop-cart.svg';
+import './scss/App.scss';
+import './scss/normalize.scss';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='app'>
+      <header className='header'>
+        <div className='header__logoBlock'>
+          <img src={pizzaLogo} alt='Пицца' />
+          <div className='header__titleBlock'>
+            <span className='header__mainText'>pizza for all</span>
+            <span className='header__secondaryText'>
+              самая вкусная пицца во вселенной
+            </span>
+          </div>
+        </div>
+        <div className='header__cartButton'>
+          <span className='header__price'>520 ₽</span>
+          <div className='header__vl'></div>
+          <img src={shopCart} alt='Корзина' />
+          <span className='header__countItems'>3</span>
+        </div>
       </header>
     </div>
   );
-}
+};
 
 export default App;
